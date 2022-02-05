d6489a089cdc72213c2dccba55a6b950430281a0
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2022-02-05 19:41:17 +0900

    Initial Commit

---
+++ RSSReader/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RSSReader
+//  
+//  Created by kunikuni03 on 2022/02/05
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RSSReader/RSSReaderApp.swift
@@ -0,0 +1,18 @@
+//
+//  RSSReaderApp.swift
+//  RSSReader
+//  
+//  Created by kunikuni03 on 2022/02/05
+//  
+//
+
+import SwiftUI
+
+@main
+struct RSSReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
