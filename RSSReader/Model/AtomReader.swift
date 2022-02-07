105dbf2ad009af3a28acc843314c41ec596b842a
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2022-02-07 23:46:26 +0900

    load xml file

--- RSSReader/Model/AtomReader.swift
+++ RSSReader/Model/AtomReader.swift
@@ -9,9 +9,8 @@
 import Foundation
 
 class AtomReader: NSObject {
-    
     private var parser: XMLParser!
-    
+
     init(data: Data) {
         self.parser = XMLParser(data: data)
         super.init()
@@ -36,4 +35,20 @@ extension AtomReader: XMLParserDelegate {
     func parserDidEndDocument(_ parser: XMLParser) {
         print("End to parse for Atom file.")
     }
+    
+    // When parsing the start tag.
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        print("When parsing the start tagName: \(elementName)")
+    }
+    
+    // When parsing the element.
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        print("When parsing the element: \(string)")
+    }
+    
+    // When parsing the end tag.
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        print("When parsing the end tagName: \(elementName)")
+    }
+
 }
--- RSSReader/View/RSSContentView.swift
+++ RSSReader/View/RSSContentView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 
 struct RSSContentView: View {
     @EnvironmentObject var settings :SettingModel
+    
 
     var body: some View {
         VStack {
             Text("Content menu")
             Text("url is: \(settings.url)")
+            Button {
+                withAnimation {
+                    xmlLoad()
+                }
+            } label: {
+                Text("XMLファイル読み込み")
+            }
         }
+    }
+    
+    func xmlLoad() {
+        guard let url = Bundle.main.url(forResource: "testAtom", withExtension: "xml") else {
+            print("file not found.")
+            return
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            print("load error.")
+            return
+        }
+        let reader = AtomReader(data: data)
+        reader.parse()
     }
 }
 
